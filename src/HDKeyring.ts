4c31f4e9b9ce2d4b328920fa1a1cd41272a73323
Monte Lai <monte.lai@consensys.net>
2023-03-17 15:59:15 +0800

    set default hdpath

--- src/HDKeyring.ts
+++ src/HDKeyring.ts
@@ -64,7 +64,7 @@ export default class HdKeyring implements Keyring<SerializedHdKeyringState> {
 
   hdWallet: HDKey | undefined | null;
 
-  hdPath: string | undefined | null;
+  hdPath: string = hdPathString;
 
   opts: KeyringOpt | undefined | null;
 
@@ -135,10 +135,6 @@ export default class HdKeyring implements Keyring<SerializedHdKeyringState> {
   serialize(): Promise<SerializedHdKeyringState> {
     if (!this.mnemonic) {
       throw new Error(HDKeyringErrors.MISSING_MNEMONIC);
-    }
-
-    if (!this.hdPath) {
-      throw new Error(HDKeyringErrors.MISSING_HD_PATH);
     }
 
     const mnemonicAsString = this.#uint8ArrayToString(this.mnemonic);
